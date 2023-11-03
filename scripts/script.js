82eb06b68594faadd1c4023abbc191ec52b2b454
Vladislav Konovalov <vladislav.konovalov1@yandex.ru>
2023-11-03 13:47:19 +0300

    добавлена карта и мелкие элементы

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,30 @@ navToggle.addEventListener('click', function() {
       navMain.classList.remove('main-nav--opened');
     }
 });
+
+
+initMap();
+
+async function initMap() {
+    // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
+    await ymaps3.ready;
+
+    const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+
+    // Иницилиазируем карту
+    const map = new YMap(
+        // Передаём ссылку на HTMLElement контейнера
+        document.getElementById('map'),
+        // Передаём параметры инициализации карты
+        {
+            location: {
+                // Координаты центра карты
+                center: [30.32285967962233, 59.93832533227361],
+                // Уровень масштабирования
+                zoom: 15
+            }
+        }
+    );
+    // Добавляем слой для отображения схематической карты
+    map.addChild(new YMapDefaultSchemeLayer());
+}
